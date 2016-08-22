84f5703ec6e09070aae9ed1261b63406c75eda6b
ice <smalltides@gmail.com>
2016-08-22 18:33:54 +0800

    add action and reducers import declare

--- ice/src/actions/index.js
+++ ice/src/actions/index.js
@@ -1,4 +1,9 @@
+import axios from 'axios';
 
+export const FETCH_POKEMON_INFO = 'FETCH_POKEMON_INFO';
+export const FETCH_POKEMONS_LOCATION = 'FETCH_POKEMONS_LOCATION';
+
+const GET_LOCATION = '';
 
 export function fetchPokemonInfo(){
 
