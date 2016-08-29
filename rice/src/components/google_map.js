d118ea87c72f39456c45c39fc996ea715886e8eb
bigrice0102 <yilinyeh0102@gmail.com>
2016-08-29 14:55:20 +0800

    Show selected pokemon as markers in google map

--- rice/src/components/google_map.js
+++ rice/src/components/google_map.js
@@ -1,19 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { GoogleMapLoader, GoogleMap, Marker} from 'react-google-maps';
+import { connect } from 'react-redux';
 
-export default (props) =>{
-    return (
-        <div className="col-md-12">
-            <GoogleMapLoader
-                containerElement = { <div className="embed-responsive embed-responsive-16by9" />}
-                googleMapElement = {
-                    <GoogleMap 
-                        defaultZoom={10}  
-                        defaultCenter={{ lat: 24.55, lng: 121.1 }} >
-                        // Marker Here
-                    </GoogleMap>    
-                }
-            />
-         </div>     
-    );
+class GoogleMapWithPokemons extends Component{
+    
+    renderMarkers(){
+        if (!this.props.pokemon)
+        {
+            return '';
+        }
+		return this.props.pokemon.pokemon.data.map((pokemon)=>{
+			return (
+                <Marker  
+                    key={pokemon.id} 
+                    position={{lat:pokemon.latitude, lng:pokemon.longitude}}
+                    defaultAnimation={1}         
+                />
+			);
+		});
+    }
+    
+    render(){
+      return (
+            <div>
+                <GoogleMapLoader
+                    containerElement = { <div className="embed-responsive embed-responsive-16by9" />}
+                    googleMapElement = {
+                        <GoogleMap 
+                            defaultZoom={9}  
+                            defaultCenter={{ lat: 24.9555, lng: 121.5 }} >
+                            {this.renderMarkers()}
+                        </GoogleMap>    
+                    }
+                />
+            </div>     
+        );
+    }   
 }
+
+function mapStateToProps(state){
+    return { pokemon: state.pokemon}; 
+}
+
+export default connect(mapStateToProps)(GoogleMapWithPokemons);
